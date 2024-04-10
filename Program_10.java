a10b401cc052ce6bfc3a309a1d73df6271cd44d4
qnkarthika <134907181+qnkarthika@users.noreply.github.com>
2024-04-10 19:38:44 +0530

    First Commit

--- Program_10.java
+++ Program_10.java
@@ -1,6 +1,6 @@
-10.Right rotate of array with k rotations
+10.Right and Left rotate of array with k rotations
 
-  
+//Right rotate
 import java.util.*;
 class Main {
     public static void main(String[] args) {
@@ -23,6 +23,33 @@ class Main {
        System.out.print("Array after " +rotation+ " rotation: ");
        for(int i=0;i<n;i++){
             System.out.print(l.get(i)+" ");
+        }
+    }
+}
+
+//Left rotate
+
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter size of an array: ");
+        int n = s.nextInt();
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        System.out.print("Enter array elements: ");
+        for (int i = 0; i < n; i++) {
+            l.add(s.nextInt());
+        }
+        System.out.print("Enter number of rotations: ");
+        int rotation = s.nextInt();
+
+        for (int i = 0; i < rotation; i++) {
+            int firstElement = l.remove(0);
+            l.add(n - 1, firstElement);
+        }
+        System.out.print("Array after " + rotation + " rotation: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(l.get(i) + " ");
         }
     }
 }
