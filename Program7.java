678c06074f53261d6871c6d86f1ac0c96c98a57c
qnkarthika <134907181+qnkarthika@users.noreply.github.com>
2024-04-10 19:04:45 +0530

    Create Program7.java

---
+++ Program7.java
@@ -0,0 +1,21 @@
+7. Finding the missing element from 1 to n
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter size of an array: ");
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter array elements: ");
+        for (int i = 0; i < n; i++) { // Corrected loop condition
+            arr[i] = s.nextInt();
+        }
+        int sumOfNatural = n * (n + 1) / 2;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Missing element from 1 to n is " + (sumOfNatural - sum));
+    }
+}
