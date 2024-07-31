8a075c7197f0e6ca22fc573df35d1818043e4e2c
qnkarthika <134907181+qnkarthika@users.noreply.github.com>
2024-07-31 22:44:36 +0530

    Create Program_95.java

---
+++ Program_95.java
@@ -0,0 +1,35 @@
+Write a program to get a string S, remove duplicate characters, and maintain the same order.
+
+Input 1 : Hello World
+Output 1 : Helo Wrd
+  
+Input 2 : Enjoy Moments
+Output 2 : Enjoy Mmets
+
+Solution:
+
+import java.util.*;
+class Main{
+    
+    public static String removeDuplicate(String str){
+        StringBuilder sb=new StringBuilder();
+        HashSet<Character> set=new HashSet<>();
+        for(char ch:str.toCharArray()){
+            if(ch!=' ' && !set.contains(ch)){
+                sb.append(ch);
+                set.add(ch);
+            }
+            else if(ch==' '){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        int freq[]=new int[27];
+        String res=removeDuplicate(str);
+        System.out.print(res);
+    }
+}
