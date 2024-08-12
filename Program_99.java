3c1066e16153f6c877ab850663b5c59d132e1e8b
qnkarthika <134907181+qnkarthika@users.noreply.github.com>
2024-08-12 16:21:38 +0530

    Create Program_99.java

---
+++ Program_99.java
@@ -0,0 +1,46 @@
+Trapping Rain Water
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+  
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+Solution:
+
+class Solution {
+    public int trap(int[] height) {
+         if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int waterTrapped = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return waterTrapped;
+    }
+}
